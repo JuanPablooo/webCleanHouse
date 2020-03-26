abe83d86622c8c65f1bf1e0281d9ae986d509154
yasmin57 <pereirayasmin57@gmail.com>
2020-03-26 06:55:26 -0300

    26/03/20 - continuando a pagina home

--- cleanhouse/src/components/home/Home.js
+++ cleanhouse/src/components/home/Home.js
@@ -88,7 +88,7 @@ export default class Home extends Component {
             <div className="row">
               <div className="col-md-6 d-flex border-bottom border-white">
                 <div className="justify-content-center align-self-center">
-                  <h1 className="text-capitalize mt-5 mb-5">pagamento</h1>
+                  <h1 className="text-uppercase mt-5 mb-5">pagamento</h1>
                   <p className="ml-auto mr-auto col-8 text-center mb-5">
                     Recebimento do pagamento ocorre de forma segura, diretamente
                     da plataforma. Além disso, a renda de cada serviço realizado
@@ -106,7 +106,7 @@ export default class Home extends Component {
             <div className="row">
               <div className="col-md-6 d-flex">
                 <div className="justify-content-center align-self-center">
-                  <h1 className="text-capitalize mt-5 mb-5">SERVIÇOS</h1>
+                  <h1 className="text-uppercase mt-5 mb-5">SERVIÇOS</h1>
                   <p className="ml-auto mr-auto col-9 text-center">
                     Limpeza: Empregados (as) que limpam todos os cômodos da sua
                     casa
@@ -114,12 +114,157 @@ export default class Home extends Component {
                   <p className="ml-auto mr-auto col-9 text-center">
                     Cozinha: Empregados (as) que fazem o preparo da sua comida
                   </p>
-                  <p className="ml-auto mr-auto col-9 text-center">
+                  <p className="ml-auto mr-auto col-9 text-center mb-5">
                     Roupas: Empregados (as) que lavam e passam suas roupas
                   </p>
                 </div>
               </div>
               <div className="col-md-6"></div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="container text-gray">
+            <h1 className="text-center mt-5 text-yellow">Vantagens</h1>
+
+            <div className="row">
+              <div className="col-md-12">
+                <h2 className="offset-md-1 text-left mt-3 mb-3 text-blue text-capitalize">
+                  Para o profissional
+                </h2>
+              </div>
+              <div className="col-md-12">
+                <div className="ml-auto mr-auto icon-vantagem icon-vantagem01 mb-3"></div>
+                <p className="offset-md-4 col-md-4">
+                  A cada serviço realizado, acontece uma avaliação para o
+                  profissional. Quanto mais avaliações com 5 estrelas, o
+                  profissional sobe seu ranking e pode ser premiado como mais
+                  bem colocado nas listas.
+                </p>
+              </div>
+              <div className="col-md-12 row">
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem02 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Trabalho totalmente autônomo. Sem chefes e com seus próprios
+                    horários
+                  </p>
+                </div>
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem03 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Fácil processo para ser contratado (a). Basta se cadastrar e
+                    aguardar um cliente.
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-12 row mt-5 mb-5">
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem04 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Raio de localização acessível e escolhido pelo próprio
+                    prestador de serviço.
+                  </p>
+                </div>
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem05 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Custo do trabalho é decidido pelo funcionário para que o
+                    próprio adeque o dinheiro da condução.
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-md-12">
+                <h2 className="offset-md-1 text-left mt-5 mb-3 text-blue text-capitalize">
+                  Para o cliente
+                </h2>
+              </div>
+
+              <div className="col-md-12 row mt-5">
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem06 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Em apenas alguns cliques você encontra profissionais
+                    capacitados à poucos metros de distância da sua casa.
+                  </p>
+                </div>
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem07 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Na nossa plataforma existe um sistema de avaliação, onde
+                    você pode verificar a confiabilidade de nossos
+                    colaboradores.
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-md-12 row mt-5">
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem08 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Onde você escolhe o dia e a hora que deseja que o serviço
+                    seja realizado.
+                  </p>
+                </div>
+                <div className="col-md-6">
+                  <div
+                    className="ml-auto mr-auto icon-vantagem 
+                    icon-vantagem09 mb-3"
+                  ></div>
+                  <p className="offset-md-1 col-md-10">
+                    Com preços acessíveis a Clean House satifaz os seus
+                    clientes.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="separador-de-sessao mt-5"></div>
+        </section>
+
+        <section>
+          <div className="container text-gray pb-5">
+            <h1 className="text-center mt-5 text-blue-dark mb-3">Regiões</h1>
+            <div className="row mt-5">
+              <div className="offset-md-2 col-md-4 img-regioes"></div>
+              <div className="col-md-6 row">
+                <div className="col-md-8 pl-3 mt-4 pt-1">
+                  <p>
+                    Atualmente os nossos serviços estão disponíveis apenas para
+                    a região metropolitana de São Paulo.
+                  </p>
+                  <a
+                    href="https://emplasa.sp.gov.br/RMSP"
+                    className="btn btn-primary btn-radius"
+                    target="blank"
+                  >
+                    Veja o mapa
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </section>
