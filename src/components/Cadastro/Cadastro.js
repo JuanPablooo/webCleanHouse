b3bae9accdef7d8e0e0e48463e1c8f6d06b41a46
JuanPablooo <juanpablopereiragomes@hotmail.com>
2020-04-04 15:13:22 -0300

    trocando para pagina cadastro

---
+++ src/components/Cadastro/Cadastro.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+export default function Cadastro(props){
+  const {estado, setEstado} = useState(0)
+  
+  
+  return(
+    <>
+      <h1>cadastro</h1>
+    
+    </>
+  )
+
+}
